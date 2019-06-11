67eca6b7c6760e5413b1064bf94087a3a7ce9843
Anshul <anshulgangwar63@gmail.com>
2019-06-11 11:53:59 +0530

    Added a todo app built using react

---
+++ src/AddTodo.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class AddTodo extends React.Component
+{
+    state = {
+        content:''
+    }
+    handleChange = (e) =>{
+        this.setState({
+            content:e.target.value
+        })
+    }
+    handleSubmit = (e) =>
+    {
+        e.preventDefault();
+        this.props.addTodo(this.state);
+        this.setState({
+            content:''
+        })
+    }
+
+    render()
+    {
+        return(
+            <div>
+                <form onSubmit = {this.handleSubmit}>
+                    <label>Add new Todo: </label>
+                    <input type="text" onChange = {this.handleChange} value = {this.state.content}/>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddTodo;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todo from './todo';
+import AddTodo from './AddTodo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component
+{
+  state = {
+    todos:[
+      {id:1,content:"Go to medical Shop"},
+      {id:2,content:"Pay the telephone bills"}
+    ]
+  }
+  deleteTodo = (id) =>{
+    const todos = this.state.todos.filter(todo =>{
+      return todo.id !== id;
+    })
+    this.setState({
+      todos
+    })
+  }
+
+  addTodo = (todo)=>{
+    todo.id = Math.random();
+    let todos = [...this.state.todos,todo];
+    this.setState({
+      todos
+    })
+  }
+  render(){
+    return(
+      <div className="todoapp container">
+        <h1 className="center blue-text">Todo's App</h1>
+        <Todo todos = {this.state.todos} deleteTodo = {this.deleteTodo}/>
+        < AddTodo addTodo = {this.addTodo}/>
+      </div>
+    )
+  }
 }
 
 export default App;
