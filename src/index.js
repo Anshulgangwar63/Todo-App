c24e96800ce0ae44668c680bb9a189aa64ec9502
Gangwar99 <72185476+Gangwar99@users.noreply.github.com>
2020-10-01 11:24:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,4 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
